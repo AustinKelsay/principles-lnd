2845fe31f4cd9f1aa78629a986b7a313cba712bb
austinkelsay <austinkelsay@yahoo.com>
2021-04-05 20:32:05 -0500

    Removing unneeded tsConfig

--- lightning/lightningRouter.ts
+++ lightning/lightningRouter.ts
@@ -3,29 +3,21 @@ import nodeManager from './nodeManager';
 const Nodes = require('./lightningModel')
 const router = require("express").Router();
 
-/**
- * POST /lnd/connect
- */
-// export const connect = async (req: Request, res: Response) => {
-//   const { host, cert, macaroon } = req.body;
-//   const { token, pubkey } = await nodeManager.connect(host, cert, macaroon);
-//   await db.addNode({ host, cert, macaroon, token, pubkey });
-//   res.send({ token });
-// };
-
 router.post('/connection', (req: Request, res: Response) => {
 
 })
 
-
+// Add a node to user
 router.post("/", (req: Request, res: Response) => {
-  Nodes.getAllNodes()
-  .then((nodes: any) => {
-    res.status(200).json(nodes)
-  })
-  .catch((err: any) => {
-    res.status(500).json(err)
-  })
+  if (req.body.host && req.body.cert && req.body.macaroon && req.body.pubkey) {
+    Nodes.addNode(req.body)
+    .then((nodes: any) => {
+      res.status(200).json(nodes)
+    })
+    .catch((err: any) => {
+      res.status(500).json(err)
+    })
+  }
 })
   
 module.exports = router
