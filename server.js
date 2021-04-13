78785f1b7c730b1651d8a1f21aafd550ca660197
austinkelsay <austinkelsay@yahoo.com>
2021-04-12 19:13:42 -0500

    No longer pushing dist

---
+++ server.js
@@ -0,0 +1,25 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var authRouter = require("./users/authRouter");
+var principlesRouter = require("./principles/principlesRouter");
+var userPrinciplesRouter = require("./principles/userPrinciplesRouter");
+var lightningRouter = require("./lightning/lightningRouter");
+var dotenv_1 = require("dotenv");
+var cors_1 = require("cors");
+var helmet_1 = require("helmet");
+var morgan_1 = require("morgan");
+dotenv_1["default"].config();
+var server = express_1["default"]();
+server.use(express_1["default"].json());
+server.use(cors_1["default"]());
+server.use(helmet_1["default"]());
+server.use(morgan_1["default"]('tiny'));
+server.get("/", function (req, res) {
+    res.status(200).json({ message: "Welcome!" });
+});
+server.use('/auth', authRouter);
+server.use('/principles', principlesRouter);
+server.use('/principles/user', userPrinciplesRouter);
+server.use("/lnd", lightningRouter);
+exports["default"] = server;
