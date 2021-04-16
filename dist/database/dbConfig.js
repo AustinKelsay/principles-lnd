44630a3e45776050d9053483bf70fe302f22faee
austinkelsay <austinkelsay@yahoo.com>
2021-04-16 10:28:47 -0500

    Worked on .env some more

--- dist/database/dbConfig.js
+++ dist/database/dbConfig.js
@@ -1,5 +1,6 @@
 "use strict";
 var knex = require("knex");
+require('dotenv').config();
 var config = require("../knexfile");
 var env = process.env.DB_ENV || "development";
 module.exports = knex(config[env]);
